37b44c18edf4642d9cf45384b3f2a6c1e877b612
Thomas Wiens <th.wiens@gmx.de>
2023-07-03 23:39:20 +0200

    Add SetVariableRequest and SetVariableResponse function codes

---
+++ src/S7CommPlusDriver/Core/SetVariableRequest.cs
@@ -0,0 +1,100 @@
+﻿#region License
+/******************************************************************************
+ * S7CommPlusDriver
+ * 
+ * Copyright (C) 2023 Thomas Wiens, th.wiens@gmx.de
+ *
+ * This file is part of S7CommPlusDriver.
+ *
+ * S7CommPlusDriver is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ /****************************************************************************/
+#endregion
+
+using System;
+using System.IO;
+
+namespace S7CommPlusDriver
+{
+    public class SetVariableRequest : IS7pSendableObject
+    {
+        public byte ProtocolVersion;
+        public UInt16 SequenceNumber;
+        public UInt32 SessionId;
+        byte TransportFlags = 0x34;
+
+        public UInt32 InObjectId;
+
+        public UInt32 Address;
+        public PValue Value;
+
+        public bool WithIntegrityId = true;
+        public UInt32 IntegrityId;
+
+        public SetVariableRequest(byte protocolVersion)
+        {
+            ProtocolVersion = protocolVersion;
+        }
+
+        public byte GetProtocolVersion()
+        {
+            return ProtocolVersion;
+        }
+
+        public int Serialize(Stream buffer)
+        {
+            UInt32 i;
+            int ret = 0;
+            ret += S7p.EncodeByte(buffer, Opcode.Request);
+            ret += S7p.EncodeUInt16(buffer, 0);                               // Reserved
+            ret += S7p.EncodeUInt16(buffer, Functioncode.SetVariable);
+            ret += S7p.EncodeUInt16(buffer, 0);                               // Reserved
+            ret += S7p.EncodeUInt16(buffer, SequenceNumber);
+            ret += S7p.EncodeUInt32(buffer, SessionId);
+            ret += S7p.EncodeByte(buffer, TransportFlags);
+
+            // Request set
+            ret += S7p.EncodeUInt32(buffer, InObjectId);
+            ret += S7p.EncodeUInt32Vlq(buffer, 1); // Immer 1 (?)
+            ret += S7p.EncodeUInt32Vlq(buffer, Address);
+            ret += Value.Serialize(buffer);
+
+            ret += S7p.EncodeObjectQualifier(buffer);
+            // 1 Byte unbekannter Funktion
+            ret += S7p.EncodeByte(buffer, 0x00);
+
+            if (WithIntegrityId)
+            {
+                ret += S7p.EncodeUInt32Vlq(buffer, IntegrityId);
+            }
+
+            // Füllbytes?
+            ret += S7p.EncodeUInt32(buffer, 0);
+
+            return ret;
+        }
+
+        public override string ToString()
+        {
+            string s = "";
+            s += "<SetVariableRequest>" + Environment.NewLine;
+            s += "<ProtocolVersion>" + ProtocolVersion.ToString() + "</ProtocolVersion>" + Environment.NewLine;
+            s += "<SequenceNumber>" + SequenceNumber.ToString() + "</SequenceNumber>" + Environment.NewLine;
+            s += "<SessionId>" + SessionId.ToString() + "</SessionId>" + Environment.NewLine;
+            s += "<TransportFlags>" + TransportFlags.ToString() + "</TransportFlags>" + Environment.NewLine;
+            s += "<RequestSet>" + Environment.NewLine;
+            s += "<InObjectId>" + InObjectId.ToString() + "</InObjectId>" + Environment.NewLine;
+            s += "<AddressList>" + Environment.NewLine;
+            s += "<Id>" + Address.ToString() + "</Id>" + Environment.NewLine;
+            s += "</AddressList>" + Environment.NewLine;
+            s += "<ValueList>" + Environment.NewLine;
+            s += "<Value>" + Value.ToString() + "</Value>" + Environment.NewLine;
+            s += "</ValueList>" + Environment.NewLine;
+            s += "</RequestSet>" + Environment.NewLine;
+            s += "</SetVariableRequest>" + Environment.NewLine;
+            return s;
+        }
+    }
+}
---
+++ src/S7CommPlusDriver/Core/SetVariableResponse.cs
@@ -0,0 +1,89 @@
+﻿#region License
+/******************************************************************************
+ * S7CommPlusDriver
+ * 
+ * Copyright (C) 2023 Thomas Wiens, th.wiens@gmx.de
+ *
+ * This file is part of S7CommPlusDriver.
+ *
+ * S7CommPlusDriver is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ /****************************************************************************/
+#endregion
+
+using System;
+using System.IO;
+
+namespace S7CommPlusDriver
+{
+    public class SetVariableResponse
+    {
+        public byte ProtocolVersion;
+        public UInt16 SequenceNumber;
+        public byte TransportFlags;
+        public UInt64 ReturnValue;
+        public UInt32 IntegrityId;
+
+        public SetVariableResponse(byte protocolVersion)
+        {
+            ProtocolVersion = protocolVersion;
+        }
+
+        public int Deserialize(Stream buffer)
+        {
+            int ret = 0;
+
+            ret += S7p.DecodeUInt16(buffer, out SequenceNumber);
+            ret += S7p.DecodeByte(buffer, out TransportFlags);
+
+            // Response Set
+            ret += S7p.DecodeUInt64Vlq(buffer, out ReturnValue);
+
+            ret += S7p.DecodeUInt32Vlq(buffer, out IntegrityId);
+            return ret;
+        }
+
+        public override string ToString()
+        {
+            string s = "";
+            s += "<SetVariableResponse>" + Environment.NewLine;
+            s += "<ProtocolVersion>" + ProtocolVersion.ToString() + "</ProtocolVersion>" + Environment.NewLine;
+            s += "<SequenceNumber>" + SequenceNumber.ToString() + "</SequenceNumber>" + Environment.NewLine;
+            s += "<TransportFlags>" + TransportFlags.ToString() + "</TransportFlags>" + Environment.NewLine;
+            s += "<ResponseSet>" + Environment.NewLine;
+            s += "<ReturnValue>" + ReturnValue.ToString() + "</ReturnValue>" + Environment.NewLine;
+            s += "</ResponseSet>" + Environment.NewLine;
+            s += "<IntegrityId>" + IntegrityId.ToString() + "</IntegrityId>" + Environment.NewLine;
+            s += "</SetVariableResponse>" + Environment.NewLine;
+            return s;
+        }
+
+        public static SetVariableResponse DeserializeFromPdu(Stream pdu)
+        {
+            byte protocolVersion;
+            byte opcode;
+            UInt16 function;
+            UInt16 reserved;
+            // ProtocolVersion wird vorab als ein Byte in den Stream geschrieben, Sonderbehandlung
+            S7p.DecodeByte(pdu, out protocolVersion);
+            S7p.DecodeByte(pdu, out opcode);
+            if (opcode != Opcode.Response)
+            {
+                return null;
+            }
+            S7p.DecodeUInt16(pdu, out reserved);
+            S7p.DecodeUInt16(pdu, out function);
+            S7p.DecodeUInt16(pdu, out reserved);
+            if (function != Functioncode.SetVariable)
+            {
+                return null;
+            }
+            SetVariableResponse resp = new SetVariableResponse(protocolVersion);
+            resp.Deserialize(pdu);
+
+            return resp;
+        }
+    }
+}
