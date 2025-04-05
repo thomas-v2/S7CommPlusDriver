7833d03c3cda561bb8f7c729845ae2c7dbec8f91
Ich-Eben <mail@icheben.com>
2025-04-05 17:58:02 +0200

    fix indentations

--- src/S7CommPlusDriver/Net/S7Client.cs
+++ src/S7CommPlusDriver/Net/S7Client.cs
@@ -386,17 +386,17 @@ namespace S7CommPlusDriver
 			return _LastError;
 		}
 
-        public byte[] getOMSExporterSecret()
-        {
-            if (m_sslconn == null) return null;
-            return m_sslconn.getOMSExporterSecret();
-        }
+		public byte[] getOMSExporterSecret()
+		{
+			if (m_sslconn == null) return null;
+			return m_sslconn.getOMSExporterSecret();
+		}
 
-        #endregion
+		#endregion
 
-        #region [Class Control]
+		#region [Class Control]
 
-        public S7Client()
+		public S7Client()
 		{
 			m_DateTimeStarted = DateTime.Now;
 			CreateSocket();
@@ -584,8 +584,8 @@ namespace S7CommPlusDriver
 				case S7Consts.errCliInvalidBlockSize: return "CLI : Invalid block size";
 				case S7Consts.errCliNeedPassword: return "CPU : Function not authorized for current protection level";
 				case S7Consts.errCliInvalidPassword: return "CPU : Invalid password";
-                case S7Consts.errCliAccessDenied: return "CPU : Access denied";
-                case S7Consts.errCliNoPasswordToSetOrClear: return "CPU : No password to set or clear";
+				case S7Consts.errCliAccessDenied: return "CPU : Access denied";
+				case S7Consts.errCliNoPasswordToSetOrClear: return "CPU : No password to set or clear";
 				case S7Consts.errCliJobTimeout: return "CLI : Job Timeout";
 				case S7Consts.errCliFunctionRefused: return "CLI : function refused by CPU (Unknown error)";
 				case S7Consts.errCliPartialDataRead: return "CLI : Partial data read";
@@ -594,9 +594,9 @@ namespace S7CommPlusDriver
 				case S7Consts.errCliInvalidParamNumber: return "CLI : Invalid Param Number";
 				case S7Consts.errCliCannotChangeParam: return "CLI : Cannot change this param now";
 				case S7Consts.errCliFunctionNotImplemented: return "CLI : Function not implemented";
-                case S7Consts.errCliFirmwareNotSupported: return "CLI : Firmware not supported";
-                case S7Consts.errCliDeviceNotSupported: return "CLI : Device type not supported";
-                default: return "CLI : Unknown error (0x" + Convert.ToString(Error, 16) + ")";
+				case S7Consts.errCliFirmwareNotSupported: return "CLI : Firmware not supported";
+				case S7Consts.errCliDeviceNotSupported: return "CLI : Device type not supported";
+				default: return "CLI : Unknown error (0x" + Convert.ToString(Error, 16) + ")";
 			};
 		}
 
